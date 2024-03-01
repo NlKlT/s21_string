b1c56d4d965f7077ed858eea5459c30d953ad1c3
Alexander Vorobyov <aovorobyov@mail.ru>
2024-03-01 20:56:16 +0300

    corriele: Added functions: s21_strrchr, s21_strstr, s21_strtok. Updated function s21_strpbrk

--- s21_string.h
+++ s21_string.h
@@ -1,3 +1,6 @@
 #include <stdio.h>
 
-char *strpbrk(const char *str1, const char *str2);
+char *s21_strpbrk(const char *str1, const char *str2);
+char *s21_strrchr(const char *str, int c);
+char *s21_strstr(const char *haystack, const char *needle);
+char *s21_strtok(char *str, const char *delim);
--- s21_strpbrk.c
+++ s21_strpbrk.c
@@ -1,29 +1,16 @@
 /*Finds the first character in the string str1 that matches any character
  * specified in str2.*/
-
 #include "s21_string.h"
 
 char *s21_strpbrk(const char *str1, const char *str2) {
-  size_t len = 0;  // заменить на s21_size_t
-  while (str2[len] != '\0') {
-    len++;
-  }
-
   while (*str1 != '\0') {
-    for (size_t i = 0; i < len; i++) {  // s21_size_t
+    for (s21_size_t i = 0; i < s21_strlen(str2);
+         i++) {  // s21_strlen, s21_size_t
       if (*str1 == str2[i]) {
         return (char *)str1;
       }
     }
     str1++;
   }
-  return NULL;  // s21_NULL
+  return s21_NULL;  // s21_NULL
 }
-
-// #include <string.h>
-// int main() {
-//   const char *str1 = "creamrrr";
-//   const char *str2 = "a";
-//   printf("%s\n", strpbrk(str1, str2));
-//   printf("%s", s21_strpbrk(str1, str2));
-// }
