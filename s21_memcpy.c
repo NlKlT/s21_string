b06cf5502efb85b54def1919e4d7acdbb5e458a0
layderwe <tputoh1@gmail.com>
2024-03-01 19:57:27 +0300

    first memcpy

---
+++ s21_memcpy.c
@@ -0,0 +1,10 @@
+#include "s21_string.h"
+
+void *s21_memcpy(void *dest, const void *src, s21_size_t n) {
+    char *csrc = (char *)src;
+    char *cdest = (char *)dest;
+    for (size_t i = 0; i < n; i++) {
+        cdest[i] = csrc[i];
+    }
+    return dest;
+}
