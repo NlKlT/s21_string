6dab660d40b1c27822b14db3bb47f4bbc7e3948f
dmitrysenko7508 <dmitrysenko7508@yandex.ru>
2024-03-07 23:04:49 +0300

    удалил свои лишние файлы

--- sprintf/flag_check.h
+++
@@ -1,4 +0,0 @@
-int minus_flag = 0, plus_flag = 0, zero_flag = 0, hashtag_flag = 0,
-    space_flag = 0;
-
-int flag_check(char *str, char flag);
--- sprintf/format_length.h
+++
@@ -1,4 +0,0 @@
-#include <ctype.h>
-int length_as_parametr = 0;
-
-int format_length(char *str, char *flag);
