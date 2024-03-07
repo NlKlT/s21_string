b88574a1c2428d57d9c27c2e7c1d03091baa6615
dmitrysenko7508 <dmitrysenko7508@yandex.ru>
2024-03-07 21:00:15 +0300

    fixed 1.0

--- s21_strchr.c
+++ s21_strchr.c
@@ -1,7 +1,7 @@
 char *s21_strchr(const char *str, int c) {
-  size_t len = s21_strlen(str);
+  s21_size_t len = strlen(str);
   char *ans = NULL;
-  for (size_t i = 0; i < len; i++) {
+  for (s21_size_t i = 0; i < len; i++) {
     if (str[i] == c) {
       ans = (char *)(i + str);
       break;
--- s21_strncmp.c
+++ s21_strncmp.c
@@ -1,6 +1,6 @@
-int s21_strncmp(const char *str1, const char *str2, size_t n) {
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n) {
   int ans = 0;
-  for (size_t i = 0; i < n; i++) {
+  for (s21_size_t i = 0; i < n; i++) {
     if (str1[i] != str2[i]) {
       ans = (int)str1[i] - (int)str2[i];
       break;
