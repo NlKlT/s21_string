94b3991dc3dc0c650fc6060efa5fb43ca72dd3cf
dmitrysenko7508 <dmitrysenko7508@yandex.ru>
2024-03-08 20:01:23 +0300

    +2 fixed functions

--- s21_strlen.c
+++ s21_strlen.c
@@ -1,3 +1,5 @@
+#include "s21_string.h"
+
 s21_size_t s21_strlen(const char *str) {
   s21_size_t res = 0;
   char *p = (char *)str;
--- s21_strncpy.c
+++ s21_strncpy.c
@@ -1,3 +1,5 @@
-char *s21_strncpy(char *dest, const char *src, size_t n) {
+#include "s21_string.h"
+
+char *s21_strncpy(char *dest, const char *src, s21_size_t n) {
   return (char *)s21_memcpy(dest, src, n);
 }
