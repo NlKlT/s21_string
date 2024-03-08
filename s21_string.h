7230cfa1a1753a4d901b37967111dc4c3483e543
dmitrysenko7508 <dmitrysenko7508@yandex.ru>
2024-03-08 21:49:40 +0300

    исправил определения нескольких функций

--- s21_string.h
+++ s21_string.h
@@ -26,14 +26,14 @@ typedef unsigned long s21_size_t;
 void *s21_memchr(const void *str, int c, s21_size_t n);
 int s21_memcmp(const void *str1, const void *str2, size_t n);
 void *s21_memcpy(void *dest, const void *src, s21_size_t n);
-void *s21_memset(void *str, int c, size_t n);
+void *s21_memset(void *str, int c, s21_size_t n);
 char *s21_strchr(const char *str, int c);  // для работы нужна функция s21_strlen
 s21_size_t s21_strcspn(const char *str1, const char *str2);
 char *s21_strerror(int errnum);
 s21_size_t s21_strlen(const char *str);
-char *s21_strncat(char *dest, const char *src, size_t n);  // для работы нужна функция s21_strlen
-int s21_strncmp(const char *str1, const char *str2, size_t n);
-char *s21_strncpy(char *dest, const char *src, size_t n);
+char *s21_strncat(char *dest, const char *src, s21_size_t n);  // для работы нужна функция s21_strlen
+int s21_strncmp(const char *str1, const char *str2, s21_size_t n);
+char *s21_strncpy(char *dest, const char *src, s21_size_t n);
 char *s21_strpbrk(const char *str1, const char *str2);
 char *s21_strrchr(const char *str, int c);
 char *s21_strstr(const char *haystack, const char *needle);
