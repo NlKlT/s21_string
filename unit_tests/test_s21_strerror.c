2a09356632276b932254e0f397117297df6af4c2
dmitrysenko7508 <dmitrysenko7508@yandex.ru>
2024-03-09 20:49:31 +0300

    changed Functions checking to strerror

--- unit_tests/test_s21_strerror.c
+++ unit_tests/test_s21_strerror.c
@@ -23,7 +23,7 @@ START_TEST(test_s21_strerror_4) {
 END_TEST
 
 Suite *make_strerror_suite(void) {
-  Suite *s = suite_create("Functions checking");
+  Suite *s = suite_create("strerror");
   TCase *tc_strerror = tcase_create("strerror_test");
 
   suite_add_tcase(s, tc_strerror);
